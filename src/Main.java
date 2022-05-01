cc027aac69d2a6a2c3b3cbad7cc4a07e65fd8164
Антон Илларионов <anton.illarionov1991@gmail.com>
2022-05-01 19:40:21 +0300

    QA3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 200_000;
+        int miles = service.calculate(price);
+        System.out.println("Бонусные мили : " + miles);
+    }
+}
